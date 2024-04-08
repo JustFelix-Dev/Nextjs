5efe0637d15c1a6b157c327935be16fe000aa39b
Felix <owolabifelix78@gmail.com>
2024-04-08 16:32:52 +0100

    nextjs layout and routes

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import Link from 'next/link';
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="border shadow-lg flex justify-between">
+    <nav className="flex justify-between">
+      <div>
+        <Image
+          src="/logo.png"
+          width={60}
+          height={60}
+          alt="my-logo"
+          placeholder="blur"
+          blurDataURL="hello"
+          quality={100}
+        />
+      </div>
       <h1>Felix-HelpDesk</h1>
       <div className="flex gap-2">
         <Link href="/">Dashboard</Link>
@@ -11,6 +23,6 @@ const Navbar = () => {
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
